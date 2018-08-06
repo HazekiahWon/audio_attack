3ee434996d44df2a0a3c33a1e1b2b347f5b0f797
HazekiahWon <patrickwhz1022@gmail.com>
2018-08-06 16:49:16 +0800

    fix bug in myctc.cc

--- myctc/myctc.cc
+++ myctc/myctc.cc
@@ -19,7 +19,12 @@ REGISTER_OP("Myctc")
 class MyctcOp : public OpKernel {
 public:
     explicit MyctcOp(OpKernelConstruction *context) : OpKernel(context) {}
-
+    int max(int a, int b) {
+        return a>b?a:b;
+    }
+    int min(int a, int b) {
+        return a>b?b:a;
+    }
     void Compute(OpKernelContext *context) override {
         // grab the inputs
         const Tensor &input1 = context->input(0);
@@ -56,8 +61,8 @@ public:
         llForward += log(c);
 
         for (int t=1; t<T; ++t) {
-            auto start = std::max(0, L-2*(T-t));
-            auto end = std::min(2*t+2, L);
+            auto start = max(0, L-2*(T-t));
+            auto end = min(2*t+2, L);
 
             c = 0;
             for (int s=start; s<L; ++s) {
@@ -71,7 +76,7 @@ public:
                 } else if ((s==1) or (phrase(l)==phrase(l-1))) {
                     alphas[s][t] = (alphas[s][t - 1] + alphas[s - 1][t - 1]) * logits(phrase(l), t);
                 } else {
-                    alphas[s][t] = (alphas[s][t - 1] + alphas[s - 1][t - 1] + alphas[s - 2][t - 1])*logits(seq(l), t);
+                    alphas[s][t] = (alphas[s][t - 1] + alphas[s - 1][t - 1] + alphas[s - 2][t - 1])*logits(phrase(l), t);
                 }
 
                 c += alphas[s][t];
@@ -85,13 +90,13 @@ public:
 
             for (int tmp=0; tmp<L; ++tmp) {
 //                auto t_vec = logits(t);
-                vector<float> vec(n_vocab);
+                std::vector<float> vec(n_vocab);
                 for (int i = 0; i < n_vocab; ++i)
                     vec[i] = logits(t, i);
                 auto target = vec[tmp];
                 float v{0};
                 for (auto e: vec)
-                    v += std::max(0, e - target);
+                    v += max(0, e - target);
                 myloss += alphas[tmp][t] * v;
             }
         }
